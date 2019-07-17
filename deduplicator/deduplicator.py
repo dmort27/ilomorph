00832b437cf2e076a26b8e0f240dcd60e49a0899
David R. Mortensen <dmortens@cs.cmu.edu>
2019-07-17 09:24:44 -0400

    Added comments

--- deduplicator/deduplicator.py
+++ deduplicator/deduplicator.py
@@ -1,8 +1,14 @@
 import re
 
+# Define regular expressions for consonants and vowels
 C = 'p|t|k|b|d|g|ts|ti|di|s|si|h|m|n|ni|ng|l|li|r|rr|w|y'
 V = 'a|e|i|o|u'
 
+# Compile regular expressions to match reduplication patterns
+CVC = re.compile('(?P<red>(?:{}|)(?:{})(?:{}))-?(?P=red)'.format(C, V, C))
+CV  = re.compile('(?P<red>(?:{}|)(?:{}))-?(?P=red)'.format(C, V))
+
+# Encode lexical exceptions
 EXCEPTIONAL = dict([
     ('babbaket', '<RED>baket'),
     ('babbalasang', '<RED>balasang'),
@@ -22,13 +28,13 @@ EXCEPTIONAL = dict([
     ('ubbing', '<RED>ubing'),
 ])
 
-CVC = re.compile('(?P<red>(?:{}|)(?:{})(?:{}))-?(?P=red)'.format(C, V, C))
-CV  = re.compile('(?P<red>(?:{}|)(?:{}))-?(?P=red)'.format(C, V))
-
 def dedup(token):
+    # If the token is in the list of exceptions, output the listed value
     if token in EXCEPTIONAL:
         return EXCEPTIONAL[token]
+    # If the token matches a CVC reduplication pattern, apply this substitution
     elif CVC.match(token):
         return CVC.sub(r'<RED>\g<red>', token)
+    # Otherwise, try a CV reduplication pattern
     else:
         return CV.sub(r'<RED>\g<red>', token)
